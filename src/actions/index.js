dc8e8b464e53a3cb89b1bb1e1acae38e75483fae
Gabriel Donadel <gabidonadel1@gmail.com>
2019-10-06 17:04:27 -0300

    Fix actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,62 +8,28 @@ export const TRIPS_DATA_ERROR = 'trpisDataError';
 
 const BASE_URL = 'http://www.transparencia.gov.br/api-de-dados/';
 
-export const fetchAgencies = () => {
+export const fetchAgencies = a => {
   console.log('aaa');
   return dispatch => {
     api('orgaos-siafi?descricao=santa%20catarina&pagina=1').then(res => {
+      console.log(res);
       dispatch({
         type: AGENCIES_DATA_SUCCESS,
-        payload: res.data,
+        payload: res,
       });
     });
   };
 };
 
-export const api = async url => {
-  try {
-    const res = await fetch(BASE_URL + url, {
-      method: 'GET',
+export const api = url => {
+  return fetch(BASE_URL + url, {
+    method: 'GET',
+  })
+    .then(res => {
+      return res.json().then(r => r);
+    })
+    .catch(err => {
+      console.log(err);
+      return [];
     });
-    JSON.parse(res);
-    console.log(' aqui', res.json(), res);
-    return res.json();
-  } catch (err) {
-    console.log(err);
-    console.log(' aqui2');
-    return {};
-  }
 };
-
-// export const fetchAgencies = () => {
-//   return dispatch => {
-//     try {
-//       fetch(BASE_URL + 'orgaos-siafi?descricao=santa%20catarina&pagina=1', {
-//         method: 'GET',
-//       }).then(res => console.log(res));
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     dispatch({
-//       type: AGENCIES_DATA_SUCCESS,
-//       payload: [
-//         {
-//           codigo: '26219',
-//           descricao: 'Centro Federal de Educação Tecnológica de Santa Catarina',
-//           codigoDescricaoFormatado:
-//             '26219 - Centro Federal de Educação Tecnológica de Santa Catarina',
-//         },
-//       ], //res.data,
-//     });
-//   };
-// };
-
-// export const fetchAgencies = () => async dispatch => {
-//   const res = await fetch(BASE_URL + 'orgaos-siafi?descricao=santa%20catarina&pagina=1', {
-//       method: 'GET',
-//   })
-//   dispatch({
-//     type: AGENCIES_DATA_SUCCESS,
-//     payload: res.data,
-//   });
-// };
